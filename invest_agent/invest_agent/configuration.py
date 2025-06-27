a3204070bbe1b4c4f4a24d5c31064b30ef822972
mehdi <contact@mehdi.cafe>
2025-06-28 00:21:15 +0200

    chore: Add anvil container for integration tests (#8)

--- invest_agent/invest_agent/configuration.py
+++ invest_agent/invest_agent/configuration.py
@@ -3,8 +3,6 @@ from environs import env
 
 class Configuration:
     def __init__(self):
-        env.read_env()
-
         self.agent_name = env.str("AGENT_NAME")
         self.agent_seed = env.str("AGENT_SEED")
         self.agent_port = env.int("AGENT_PORT")
