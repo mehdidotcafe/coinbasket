0de51056bffbacea3ee2aecbc66177f3c818cf70
mehdi <contact@mehdi.cafe>
2025-10-26 20:10:52 +0100

    chore(asset-swap-price): Add integration tests (#21)
    * chore(asset-swap-price): Add integration tests
    
    * fix: test_integration_asset_swap_price_sell_basket_buy_token integration test

---
+++ invest_agent/invest_agent/integration_test/asset_swap_price_integration_test.py
@@ -0,0 +1,77 @@
+import requests
+from environs import env
+from protocol.fixture.token import btc_token, eth_token
+from protocol.fixture.basket import big4_basket, memecoinmania_basket
+from syrupy.filters import paths
+
+agent_port = env.int("AGENT_PORT")
+agent_key = env.str("AGENT_KEY")
+
+
+def test_integration_asset_swap_price_sell_token_buy_token(snapshot):
+    response = requests.post(
+        f"http://localhost:{agent_port}/asset/swap/price",
+        json={
+            "sell_asset": btc_token.to_dict(),
+            "sell_asset_amount": "1",
+            "buy_asset": eth_token.to_dict(),
+            "agent_key": agent_key,
+        },
+        timeout=60,
+    )
+
+    response_json = response.json()
+
+    assert response.status_code == 200
+    assert response_json == snapshot(exclude=paths("buy_balance.amount"))
+
+
+def test_integration_asset_swap_price_sell_token_buy_basket(snapshot):
+    response = requests.post(
+        f"http://localhost:{agent_port}/asset/swap/price",
+        json={
+            "sell_asset": btc_token.to_dict(),
+            "sell_asset_amount": "1",
+            "buy_asset": big4_basket.to_dict(),
+            "agent_key": agent_key,
+        },
+        timeout=60,
+    )
+
+    response_json = response.json()
+
+    assert response.status_code == 200
+    assert response_json == snapshot(exclude=paths("buy_balance.amount"))
+
+
+def test_integration_asset_swap_price_sell_basket_buy_token(snapshot):
+    response = requests.post(
+        f"http://localhost:{agent_port}/asset/swap/price",
+        json={
+            "sell_asset": big4_basket.to_dict(),
+            "sell_asset_amount": "1",
+            "buy_asset": eth_token.to_dict(),
+            "agent_key": agent_key,
+        },
+        timeout=60,
+    )
+
+    response_json = response.json()
+
+    assert response.status_code == 200
+    assert response_json == snapshot(exclude=paths("buy_balance.amount"))
+
+
+def test_integration_asset_swap_price_sell_basket_buy_basket():
+    response = requests.post(
+        f"http://localhost:{agent_port}/asset/swap/price",
+        json={
+            "sell_asset": big4_basket.to_dict(),
+            "sell_asset_amount": "1",
+            "buy_asset": memecoinmania_basket.to_dict(),
+            "agent_key": agent_key,
+        },
+        timeout=60,
+    )
+
+    assert response.status_code == 500
