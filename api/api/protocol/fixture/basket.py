3c3f73bf85880a1034da9ccf86a7f049fa6aa55b
mehdidotcafe <contact@mehdi.cafe>
2026-01-21 17:08:04 +0100

    feat(dto): Only rely on asset.id for DTOs

--- api/api/protocol/fixture/basket.py
+++ api/api/protocol/fixture/basket.py
@@ -1,7 +1,7 @@
 from api.protocol.basket import Basket
 
 test_basket = Basket(
-    id="0d83917d-a2bd-4482-83e6-68d52c8f293a",
+    id="bsc:0x0000000000000000000000000000000000000000",
     name="Test Basket",
     display_name="Test Basket",
     ticker="TEST",
@@ -12,7 +12,7 @@ test_basket = Basket(
 )
 
 cmc20_basket = Basket(
-    id="0x2f8A339B5889FfaC4c5A956787cdA593b3c36867",
+    id="bsc:0x2f8A339B5889FfaC4c5A956787cdA593b3c36867",
     name="CMC20 Basket",
     display_name="CMC20 Basket",
     ticker="CMC20",
