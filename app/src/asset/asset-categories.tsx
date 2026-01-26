da91d76d2a19fe5b44cab5c4fd1f5bdaacb0e356
mehdidotcafe <contact@mehdi.cafe>
2026-01-27 00:21:12 +0100

    feat(prompts): add precraft prompts

--- app/src/asset/asset-categories.tsx
+++ app/src/asset/asset-categories.tsx
@@ -1,6 +1,6 @@
 import type { Asset } from './Asset'
 import { Badge } from '@/components/ui/badge'
-import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
 
 export function AssetCategories({
   asset,
@@ -8,26 +8,26 @@ export function AssetCategories({
   asset: Asset
 }) {
   return (
-    <Popover>
-      <PopoverTrigger asChild>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
         <Badge variant="secondary" className="cursor-pointer rounded-full aspect-square p-1">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="size-4">
             <path fillRule="evenodd" d="M4.5 2A2.5 2.5 0 0 0 2 4.5v2.879a2.5 2.5 0 0 0 .732 1.767l4.5 4.5a2.5 2.5 0 0 0 3.536 0l2.878-2.878a2.5 2.5 0 0 0 0-3.536l-4.5-4.5A2.5 2.5 0 0 0 7.38 2H4.5ZM5 6a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z" clipRule="evenodd" />
           </svg>
-
         </Badge>
-      </PopoverTrigger>
-      <PopoverContent>
-        <div className="flex flex-col gap-1">
-          {
-            asset.categories.map(category => (
-              <Badge variant="default" key={category}>
-                {category.toUpperCase()}
-              </Badge>
-            ))
-          }
-        </div>
-      </PopoverContent>
-    </Popover>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>Categories</DropdownMenuLabel>
+        {
+          asset.categories.sort(
+            (a, b) => a.localeCompare(b),
+          ).map(category => (
+            <DropdownMenuItem key={category} className="focus:bg-normal hover:bg-normal cursor-default">
+              {category}
+            </DropdownMenuItem>
+          ))
+        }
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
---
+++ app/src/chat/precraft-prompts.ts
@@ -0,0 +1,22 @@
+export default [
+  {
+    category: 'DEX prompts',
+    prompts: [
+      'Buy 0.001 Bitcoin',
+      'Invest in the CMC20 basket',
+      'Buy 10 $CAKE and 10 $UNI',
+      'Sell half my $ETH and $UNI',
+      'Trade all my Bitcoin tokens for Ethereum tokens',
+      'Buy 10 dollars of a token related to Trump',
+    ],
+  },
+  {
+    category: 'Portfolio prompts',
+    prompts: [
+      'What is the price of Bitcoin?',
+      'How much BNB my wallet has?',
+      'What memecoins are available?',
+      'Give me information about Bitcoin',
+    ],
+  },
+]
